0011ad4181da9de847e1c8b3d6dcdad35a298567
ApocalCrk <56026108+ApocalCrk@users.noreply.github.com>
2023-01-06 20:20:55 +0700

    commit

--- src/pages/FoodBazaar.tsx
+++ src/pages/FoodBazaar.tsx
@@ -3,7 +3,11 @@ import Layout from "../components/Layout";
 
 const FoodBazaar: FC = () => {
     return (
-        
+        <Layout>
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="text-4xl">FoodBazaar</h1>
+            </div>
+        </Layout>
     );
 };
 
