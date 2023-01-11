dbbe5fed97a7c38bce8a2da734e9ae7ed57e8840
rielgbrl <rielgasy9@gmail.com>
2023-01-11 18:37:12 +0700

    add ifest store navbar

---
+++ src/pages/uiux.tsx
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+import { FC } from "react";
+
+const Uiux: FC = () => {
+  return (
+    <Layout>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl">UI / UX</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Uiux;
