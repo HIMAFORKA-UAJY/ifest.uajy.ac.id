6250ccb213da54aa9c67883544acf7e2a6495d00
BootLoopMaster636 <christhartono@hotmail.com>
2023-01-16 09:07:44 +0700

    fix cp breakpoint

--- src/components/CP.tsx
+++ src/components/CP.tsx
@@ -25,7 +25,7 @@ const CP: FC = () => {
         <div className="p-5 font-louisgeorgecafe text-base text-white">
           Ada pertanyaan? Silakan ajukan ^_^
         </div>
-        <div className="lg-flex h-[20rem] w-[48rem] lg:w-[64rem]">
+        <div className="lg-flex h-[20rem] w-[40rem] md:w-[48rem] lg:w-[64rem]">
           <Swiper
             breakpoints={{
               640: {
